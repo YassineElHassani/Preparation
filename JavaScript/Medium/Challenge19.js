527cfff222dd273bb58ac3f2c9ab186f4bf43abf
Yassine El hassani <yassine.ds.404@gmail.com>
2025-05-02 11:18:02 +0100

    Last Challenges

--- JavaScript/Medium/Challenge19.js
+++ JavaScript/Medium/Challenge19.js
@@ -1,1 +1,17 @@
 const { employees } = require('../dataset');
+
+//19. List employees with a salary above 60000
+
+function getHighEarningEmployees(employees) {
+    let employeesList = [];
+
+    for (let i = 0; i < employees.length; i++) {
+        if(employees[i].salary > 60000) {
+            employeesList.push(employees[i]);
+        }
+    }
+
+    console.log(employeesList);
+}
+
+getHighEarningEmployees(employees);
--- JavaScript/Medium/Challenge20.js
+++ JavaScript/Medium/Challenge20.js
@@ -1,1 +1,21 @@
 const { employees } = require('../dataset');
+
+//20. Find the longest-serving employee
+
+function getLongestServingEmployee(employees) {
+    let date = new Date("2050-01-01");
+    let longestServing = null;
+
+    for(let i = 0; i < employees.length; i++) {
+        let joinDate = new Date(employees[i].joinDate);
+
+        if(joinDate < date) {
+            date = joinDate;
+            longestServing = employees[i];
+        }
+    }
+
+    console.log(longestServing);
+}
+
+getLongestServingEmployee(employees);
